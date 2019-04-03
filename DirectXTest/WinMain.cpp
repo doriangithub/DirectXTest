46423515a420b4983b1b69d44ef182b96a750247
Dorian Sharevich <dorian.sharevich@gmail.com>
2019-04-02 19:44:07 -0700

    Switching to new handeling windows message sysrtem

--- DirectXTest/WinMain.cpp
+++ DirectXTest/WinMain.cpp
@@ -1,46 +1,47 @@
-#include <Windows.h>
-#include "WindowsMessageMap.h"
-#include <sstream>
+//#include <Windows.h>
+//#include "WindowsMessageMap.h"
+//#include <sstream>
+#include "Windows.h"
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	static WindowsMessageMap mm;
-	OutputDebugString(mm(msg, lParam, wParam).c_str());
-
-	switch( msg )
-	{
-	case WM_CLOSE:
-		PostQuitMessage(69);
-		break;
-	case WM_KEYDOWN:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, "Respects");
-		}
-		break;
-	case WM_KEYUP:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, "Dangerfield");
-		}
-		break;
-	case WM_CHAR:
-		{
-			static std::string title;
-			title.push_back((char)wParam);
-			SetWindowText(hWnd, title.c_str());
-		}
-		break;
-	case WM_LBUTTONDOWN:
-		{
-			POINTS pt = MAKEPOINTS(lParam);
-			std::ostringstream oss;
-			oss << "(" << pt.x << "," << pt.y << ")";
-			SetWindowText(hWnd, oss.str().c_str());
-		}
-	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
+///LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+///{
+///	static WindowsMessageMap mm;
+///	OutputDebugString(mm(msg, lParam, wParam).c_str());
+///
+///	switch( msg )
+///	{
+///	case WM_CLOSE:
+///		PostQuitMessage(69);
+///		break;
+///	case WM_KEYDOWN:
+///		if (wParam == 'F')
+///		{
+///			SetWindowText(hWnd, "Respects");
+///		}
+///		break;
+///	case WM_KEYUP:
+///		if (wParam == 'F')
+///		{
+///			SetWindowText(hWnd, "Dangerfield");
+///		}
+///		break;
+///	case WM_CHAR:
+///		{
+///			static std::string title;
+///			title.push_back((char)wParam);
+///			SetWindowText(hWnd, title.c_str());
+///		}
+///		break;
+///	case WM_LBUTTONDOWN:
+///		{
+///			POINTS pt = MAKEPOINTS(lParam);
+///			std::ostringstream oss;
+///			oss << "(" << pt.x << "," << pt.y << ")";
+///			SetWindowText(hWnd, oss.str().c_str());
+///		}
+///	}
+///	return DefWindowProc(hWnd, msg, wParam, lParam);
+///}
 
 
 int CALLBACK WinMain(
@@ -49,40 +50,44 @@ int CALLBACK WinMain(
 	LPSTR	  lpCmdLine,
 	int       nCmdShow)
 {
-	const auto pClassName = "hw3dbutts";
+	////const auto pClassName = "hw3dbutts";
+	////
+	////// register window class
+	////WNDCLASSEX wc = {0};
+	////wc.cbSize = sizeof(wc);
+	////wc.style = CS_OWNDC;
+	////wc.lpfnWndProc = WndProc;
+	////wc.cbClsExtra = 0;
+	////wc.cbWndExtra = 0;
+	////wc.hInstance = hInstance;
+	////wc.hIcon = nullptr;
+	////wc.hCursor = nullptr;
+	////wc.hbrBackground = nullptr;
+	////wc.lpszMenuName = nullptr;
+	////wc.lpszClassName = pClassName; 
+	////wc.hIconSm = nullptr;
+	////RegisterClassEx(&wc);
+	////
+	////// create window instance
+	////HWND hWnd = CreateWindowExA(
+	////	0,
+	////	pClassName,
+	////	"Happy Hard Window",
+	////	WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+	////	200,
+	////	200,
+	////	640,
+	////	480,
+	////	nullptr,
+	////	nullptr,
+	////	hInstance,
+	////	nullptr);
+	////
+	////ShowWindow(hWnd, SW_SHOW);
 
-	// register window class
-	WNDCLASSEX wc = {0};
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName; 
-	wc.hIconSm = nullptr;
-	RegisterClassEx(&wc);
+	Window wnd(800, 300, "Window - 1 ");
+	Window wnd2(600, 400, "Window - 2 ");
 
-	// create window instance
-	HWND hWnd = CreateWindowExA(
-		0,
-		pClassName,
-		"Happy Hard Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200,
-		200,
-		640,
-		480,
-		nullptr,
-		nullptr,
-		hInstance,
-		nullptr);
-
-	ShowWindow(hWnd, SW_SHOW);
 
 	// message pump
 	MSG msg;
