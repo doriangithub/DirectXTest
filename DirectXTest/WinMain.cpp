93417f7f923a1bce8804602dc7102be4b4cf3ce3
Dorian Sharevich <dorian.sharevich@gmail.com>
2019-04-04 20:59:08 -0700

    Removed commented code

--- DirectXTest/WinMain.cpp
+++ DirectXTest/WinMain.cpp
@@ -3,47 +3,6 @@
 //#include <sstream>
 #include "Windows.h"
 
-///LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-///{
-///	static WindowsMessageMap mm;
-///	OutputDebugString(mm(msg, lParam, wParam).c_str());
-///
-///	switch( msg )
-///	{
-///	case WM_CLOSE:
-///		PostQuitMessage(69);
-///		break;
-///	case WM_KEYDOWN:
-///		if (wParam == 'F')
-///		{
-///			SetWindowText(hWnd, "Respects");
-///		}
-///		break;
-///	case WM_KEYUP:
-///		if (wParam == 'F')
-///		{
-///			SetWindowText(hWnd, "Dangerfield");
-///		}
-///		break;
-///	case WM_CHAR:
-///		{
-///			static std::string title;
-///			title.push_back((char)wParam);
-///			SetWindowText(hWnd, title.c_str());
-///		}
-///		break;
-///	case WM_LBUTTONDOWN:
-///		{
-///			POINTS pt = MAKEPOINTS(lParam);
-///			std::ostringstream oss;
-///			oss << "(" << pt.x << "," << pt.y << ")";
-///			SetWindowText(hWnd, oss.str().c_str());
-///		}
-///	}
-///	return DefWindowProc(hWnd, msg, wParam, lParam);
-///}
-
-
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
